5d8b796fa69c1128acf608125e36e829e3c5e37b
JeongEEun <wjddms2143@naver.com>
2023-02-28 01:29:45 +0900

    123

---
+++ custom.js
@@ -0,0 +1,38 @@
+// Expo를 사용하는 경우 @expo/vector-icons에서 Ionicons를 import 하거나 
+// 그렇지 않으면 react-native-vector-icons/Ionicons를 import
+import { Ionicons } from '@expo/vector-icons';
+
+// (...)
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // 여기에 원하는 컴포넌트를 반환
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
