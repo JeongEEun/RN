4b0b22a1a9abea505c27cab925ad56b1d82b81f8
JeongEEun <wjddms2143@naver.com>
2023-03-01 01:18:14 +0900

    2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+// You can import Ionicons from @expo/vector-icons if you use Expo or
+// react-native-vector-icons/Ionicons otherwise.
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
 
 function HomeScreen() {
   return (
@@ -11,10 +14,26 @@ function HomeScreen() {
   );
 }
 
-function SettingsScreen() {
+function BookAIScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text> BookAI! </Text>
+    </View>
+  );
+}
+
+function BookMarkScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> BookMark! </Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> profile!</Text>
     </View>
   );
 }
@@ -24,9 +43,35 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home';
+            } else if (route.name === 'BookAI') {
+              iconName = focused ? 'find' : 'find';
+            } else if (route.name === 'BookMark') {
+              iconName = focused ? 'star' : 'star';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'user' : 'user';
+            }
+            
+
+            // You can return any component that you like here!
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#EC6F66',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="BookAI" component={BookAIScreen} />
+        <Tab.Screen name="BookMark" component={BookMarkScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
